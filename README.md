81d83c0fa952e114ce6545bb8cf1303e32946793
Taras Pochkun <pochkun_taras@mail.ru>
2017-02-02 17:27:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Light TBlog API server
---
+++ index.js
@@ -0,0 +1,26 @@
+var express     = require('express');
+var cors        = require('cors');
+var posts       = require('./data').staticPosts;
+var _           = require('lodash');
+var application = express();
+
+
+application.use(cors());
+application.use('/public', express.static(__dirname + '/public'));
+
+application.get('/', function(req, res) {
+  res.json(posts);
+});
+
+application.get('/posts/:id', function(req, res) {
+  var id   = +req.params.id;
+  var post =  _.find(posts, { id: id });
+
+  console.log('GET: /post/' + id);
+
+  res.json({ post: post });
+});
+
+application.listen(3004, function() {
+  console.log('Server on localhost:3004');
+});
